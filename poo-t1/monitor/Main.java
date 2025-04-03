fc6f03213c9e2dc5479e0dd140af738fcdd85f09
meliton <melitonio8@hotmail.com>
2025-04-03 08:45:46 +0100

    Ok

---
+++ poo-t1/monitor/Main.java
@@ -0,0 +1,26 @@
+package monitor;
+
+class Monitor {
+    private double pulgadas;
+    private String resolucion;
+
+    public Monitor(double pulgadas, String resolucion)
+    {
+        this.pulgadas = pulgadas;
+        this.resolucion = resolucion;
+    }
+
+    public void mostrarInformacion()
+    {
+        System.out.println("Pulgadas: " + pulgadas);
+        System.out.println("Resolucion: " + resolucion);
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args) {
+        Monitor m1 = new Monitor(15.4, "1280 x 900");
+        m1.mostrarInformacion();
+    }
+}
